20712392a4df5cb43c2f2af02cae5b382dc3a342
Simone <sisilopes20@gmail.com>
2021-10-21 20:42:52 -0300

    Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json()); //necessário para recebimento do Json
+
+const customers = [];
+
+// cpf - String
+// name - string
+// id - uuid
+// statement []
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf, 
+        name,
+        id,
+        statement: []
+    })
+
+    return response.status(201).send()
+})
 
 app.listen(3333);
 
