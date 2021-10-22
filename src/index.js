73741feff01dfec56ee5b0ad1e571a974d7bdcc4
Simone <sisilopes20@gmail.com>
2021-10-21 21:13:38 -0300

    Lista extrato

--- src/index.js
+++ src/index.js
@@ -34,5 +34,14 @@ app.post('/account', (request, response) => {
     return response.status(201).send()
 })
 
+app.get('/statement/:cpf', (request, response) => {
+    const { cpf } = request.params;
+
+    //verifica se existe um extrato bancário do cliente
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+
+    return response.json(customer.statement);
+})
 app.listen(3333);
 
